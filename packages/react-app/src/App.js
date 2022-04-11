3a8e38ba0ce69af4dda055e53e0bbb98249c27fb
yiern <ernna876@gmail.com>
2022-04-11 13:01:54 +0800

    fix: change page name

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,15 +1,27 @@
 import Header from "./Components/menu";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Maze from "./Components/maze";
 import Rules from "./Components/rules";
+import { useEffect, useState } from "react";
+import RouteIndex from "./routesIndex";
 
 function App() {
+  let location = useLocation();
+  const [currPage, setCurrPage] = useState("");
+  useEffect(() => {
+    RouteIndex.map((route) => {
+      if (route.href == location.pathname) {
+        setCurrPage(route.name);
+      }
+    });
+  }, [location]);
+
   return (
     <>
       <Header></Header>
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-900">Maze</h1>
+          <h1 className="text-3xl font-bold text-gray-900">{currPage}</h1>
         </div>
       </header>
       <main>
