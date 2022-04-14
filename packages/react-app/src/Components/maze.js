6fa87a6e9b480d9314ee612352194bf741f35aac
yiern <ernna876@gmail.com>
2022-04-15 02:13:28 +0800

    add: inital maze logic

--- packages/react-app/src/Components/maze.js
+++ packages/react-app/src/Components/maze.js
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import styles from "../Styles/arrows.module.css";
+import { generateFullMaze, FullMaze, RevealedMaze } from "../testMazeLogic";
 const Display = {
   WALL: 0,
   PATH: 1,
@@ -8,132 +10,7 @@ const Display = {
   BORDER: 5,
 };
 
-const test_maze = [
-  [
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1,
-  ],
-  [
-    0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1,
-    0, 0, 0, 0, 0, 1,
-  ],
-  [
-    1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1,
-    1, 1, 1, 1, 0, 1,
-  ],
-  [
-    1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1,
-    0, 1, 0, 2, 0, 0,
-  ],
-  [
-    1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1,
-    0, 1, 1, 1, 0, 1,
-  ],
-  [
-    0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1,
-    0, 0, 0, 0, 0, 1,
-  ],
-  [
-    1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1,
-    0, 1, 1, 1, 1, 1,
-  ],
-  [
-    1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 2, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 0, 0,
-  ],
-  [
-    1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1,
-    1, 1, 1, 1, 1, 1,
-  ],
-  [
-    1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0,
-    0, 0, 0, 1, 0, 1,
-  ],
-  [
-    1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 0, 1, 0, 1,
-  ],
-  [
-    1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1,
-    0, 0, 0, 0, 0, 0,
-  ],
-  [
-    1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1,
-    1, 1, 0, 1, 1, 1,
-  ],
-  [
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 2, 0,
-    0, 0, 0, 1, 0, 0,
-  ],
-  [
-    1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 0, 1,
-  ],
-  [
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1,
-    0, 0, 0, 0, 0, 1,
-  ],
-  [
-    1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1,
-    1, 1, 1, 1, 0, 1,
-  ],
-  [
-    1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0,
-    0, 0, 0, 0, 0, 1,
-  ],
-  [
-    1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1,
-    0, 1, 0, 1, 1, 1,
-  ],
-  [
-    0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 0, 1, 0, 1,
-  ],
-  [
-    1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1,
-    1, 1, 1, 1, 0, 1,
-  ],
-  [
-    1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
-    0, 0, 0, 0, 0, 1,
-  ],
-  [
-    1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1,
-    1, 1, 1, 1, 0, 1,
-  ],
-  [
-    1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1,
-    0, 0, 0, 0, 0, 0,
-  ],
-  [
-    1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1,
-    0, 1, 1, 1, 0, 1,
-  ],
-  [
-    0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 1, 0, 1,
-  ],
-  [
-    1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 0, 1, 0, 1,
-  ],
-  [
-    0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-    0, 0, 2, 1, 0, 1,
-  ],
-  [
-    1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1,
-  ],
-  [
-    1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1,
-    0, 0, 0, 1, 0, 1,
-  ],
-  [
-    1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1,
-    1, 1, 0, 1, 0, 1,
-  ],
-];
+generateFullMaze(10, 10, 3);
 
 function renderColour(param) {
   switch (param) {
@@ -146,23 +23,31 @@ function renderColour(param) {
     case Display.START:
       return "bg-blue-500";
     case Display.HIDDEN:
-      return "bg-gray-200";
+      return "bg-gray-200 border-r border-b border-gray-300";
     default:
       return "bg-white";
   }
 }
 
 export default function Maze() {
+  const [revealedMaze, setRevealedMaze] = useState([[]]);
+  const [fullMaze, setFullMaze] = useState([[]]);
+
+  useEffect(() => {
+    setFullMaze(FullMaze.maze);
+    setRevealedMaze(RevealedMaze);
+  });
+
   return (
     <div class="flex flex-auto gap-14 flex-wrap justify-center">
-      {/* Maze */}
+      {/* Revealed Maze */}
       <div class="flex flex-col flex-nowrap align-middle">
         <div class="flex flex-nowrap justify-center h-4 xl:h-5">
-          {Array.from({ length: test_maze[0].length + 2 }, (v, index) => (
+          {Array.from({ length: revealedMaze[0].length + 2 }, (v, index) => (
             <div class=" bg-black h-4 w-4 xl:h-5 xl:w-5" key={index}></div>
           ))}
         </div>
-        {test_maze.map((row, rid) => (
+        {revealedMaze.map((row, rid) => (
           <div class="flex flex-nowrap justify-center first:overflow-visible whitespace-nowrap h-4 xl:h-5">
             <div class=" bg-black h-4 w-4 xl:h-5 xl:w-5"></div>
             {row.map((item, cid) => (
@@ -175,7 +60,32 @@ export default function Maze() {
           </div>
         ))}
         <div class="flex flex-nowrap justify-center overflow-visible whitespace-nowrap h-4 xl:h-5">
-          {Array.from({ length: test_maze[0].length + 2 }, (index) => (
+          {Array.from({ length: revealedMaze[0].length + 2 }, (index) => (
+            <div class=" bg-black h-4 w-4 xl:h-5 xl:w-5" key={index}></div>
+          ))}
+        </div>
+      </div>
+      {/* Full Maze */}
+      <div class="flex flex-col flex-nowrap align-middle">
+        <div class="flex flex-nowrap justify-center h-4 xl:h-5">
+          {Array.from({ length: fullMaze[0].length + 2 }, (v, index) => (
+            <div class=" bg-black h-4 w-4 xl:h-5 xl:w-5" key={index}></div>
+          ))}
+        </div>
+        {fullMaze.map((row, rid) => (
+          <div class="flex flex-nowrap justify-center first:overflow-visible whitespace-nowrap h-4 xl:h-5">
+            <div class=" bg-black h-4 w-4 xl:h-5 xl:w-5"></div>
+            {row.map((item, cid) => (
+              <div
+                class={` shrink-0 ${renderColour(item)} h-4 w-4 xl:h-5 xl:w-5`}
+                key={String(rid) + String(cid)}
+              ></div>
+            ))}
+            <div class=" bg-black h-4 w-4 xl:h-5 xl:w-5"></div>
+          </div>
+        ))}
+        <div class="flex flex-nowrap justify-center overflow-visible whitespace-nowrap h-4 xl:h-5">
+          {Array.from({ length: fullMaze[0].length + 2 }, (index) => (
             <div class=" bg-black h-4 w-4 xl:h-5 xl:w-5" key={index}></div>
           ))}
         </div>
@@ -183,11 +93,11 @@ export default function Maze() {
       {/* Controls */}
       <div class="flex flex-col mx-24 my-auto">
         <button class={styles.arrowUp}></button>
-        <button class="flex">
+        <div class="flex">
           <button class={styles.arrowLeft}></button>
           <div class={styles.divider}></div>
           <button class={styles.arrowRight}></button>
-        </button>
+        </div>
         <button class={styles.arrowDown}></button>
       </div>
     </div>
