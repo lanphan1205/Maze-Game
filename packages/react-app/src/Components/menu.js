2c83c518646113e15a7c978d01868cc0aafae9dd
JWIGGS <destiny.jonathan@gmail.com>
2022-04-17 00:09:39 +0800

    Added login button to menu

--- packages/react-app/src/Components/menu.js
+++ packages/react-app/src/Components/menu.js
@@ -2,13 +2,62 @@ import { Disclosure } from "@headlessui/react";
 import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 import { NavLink } from "react-router-dom";
 import RouteIndex from "../routesIndex";
+import React, { useEffect, useState } from "react";
 
 let baseNavClass = " block px-3 py-2 rounded-md text-base font-medium";
 let activeClassName = "bg-gray-900 text-white " + baseNavClass;
-let inactiveClassName =
-  "text-gray-300 hover:bg-gray-700 hover:text-white" + baseNavClass;
+let inactiveClassName = "text-gray-300 hover:bg-gray-700 hover:text-white" + baseNavClass;
+
+async function connect(onConnected) {
+  if (!window.ethereum) {
+    alert("Please install MetaMask to proceed.");
+    return;
+  }
+
+  const accounts = await window.ethereum.request({
+    method: "eth_requestAccounts",
+  });
+
+  onConnected(accounts[0]);
+}
+
+async function checkIfWalletIsConnected(onConnected) {
+  if (window.ethereum) {
+    const accounts = await window.ethereum.request({
+      method: "eth_accounts",
+    });
+
+    if (accounts.length > 0) {
+      const account = accounts[0];
+      onConnected(account);
+      return;
+    }
+  }
+}
+
+function ConnectButton({ setUserAddress }) {
+  return (
+    <button className="text-gray-300" onClick={() => connect(setUserAddress)}>
+      Login
+    </button>
+  );
+}
+
+function AddressDisplay({ userAddress }) {
+  return (
+    <text className="text-gray-300">
+      {userAddress.substring(0, 5)}…{userAddress.substring(userAddress.length - 4)}
+    </text>
+  );
+}
 
 export default function Header() {
+  const [userAddress, setUserAddress] = useState("");
+
+  useEffect(() => {
+    checkIfWalletIsConnected(setUserAddress);
+  }, []);
+
   return (
     <>
       <div className="min-h-full">
@@ -19,22 +68,12 @@ export default function Header() {
                 <div className="flex items-center justify-between h-16">
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
-                      <img
-                        className="h-8 w-8"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                        alt="Workflow"
-                      />
+                      <img className="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
                     </div>
                     <div className="hidden md:block">
                       <div className="ml-10 flex items-baseline space-x-4">
                         {RouteIndex.map((item) => (
-                          <NavLink
-                            key={item.name}
-                            to={item.href}
-                            className={({ isActive }) =>
-                              isActive ? activeClassName : inactiveClassName
-                            }
-                          >
+                          <NavLink key={item.name} to={item.href} className={({ isActive }) => (isActive ? activeClassName : inactiveClassName)}>
                             {item.name}
                           </NavLink>
                         ))}
@@ -43,21 +82,23 @@ export default function Header() {
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
-                      {/* Con add stuff here for things on the right like wallet connection*/}
+                      {
+                        /* Con add stuff here for things on the right like wallet connection*/
+                        userAddress ? (
+                          <div className="text-gray-300">
+                            Connected with <AddressDisplay userAddress={userAddress} />
+                          </div>
+                        ) : (
+                          <ConnectButton setUserAddress={setUserAddress} />
+                        )
+                      }
                     </div>
                   </div>
                   <div className="-mr-2 flex md:hidden">
                     {/* Mobile menu button */}
                     <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                       <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XIcon className="block h-6 w-6" aria-hidden="true" />
-                      ) : (
-                        <MenuIcon
-                          className="block h-6 w-6"
-                          aria-hidden="true"
-                        />
-                      )}
+                      {open ? <XIcon className="block h-6 w-6" aria-hidden="true" /> : <MenuIcon className="block h-6 w-6" aria-hidden="true" />}
                     </Disclosure.Button>
                   </div>
                 </div>
@@ -66,13 +107,7 @@ export default function Header() {
               <Disclosure.Panel className="md:hidden">
                 <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                   {RouteIndex.map((item) => (
-                    <NavLink
-                      key={item.name}
-                      to={item.href}
-                      className={({ isActive }) =>
-                        isActive ? activeClassName : inactiveClassName
-                      }
-                    >
+                    <NavLink key={item.name} to={item.href} className={({ isActive }) => (isActive ? activeClassName : inactiveClassName)}>
                       {item.name}
                     </NavLink>
                   ))}
@@ -83,9 +118,14 @@ export default function Header() {
                       <img className="h-10 w-10 rounded-full" src="" alt="" />
                     </div>
                     <div className="ml-3">
-                      <div className="text-base font-medium leading-none text-white">
-                        Put wallet address
-                      </div>
+                      {userAddress ? (
+                        <div className="text-gray-300">
+                          Connected with <AddressDisplay userAddress={userAddress} />
+                        </div>
+                      ) : (
+                        <ConnectButton setUserAddress={setUserAddress} />
+                      )}
+
                       {/* <div className="text-sm font-medium leading-none text-gray-400">
                         
                       </div> */}
